bb2fce4595ea00011838ce998dfff8e50077b650
Benjamin Dodge <benjamin.dodge@gmail.com>
2020-12-01 19:03:21 -0800

    fix (and test) for case where events for today spill over into the suncalc times of tomorrow

--- test/sunevents-test.js
+++ test/sunevents-test.js
@@ -1,11 +1,20 @@
 const timezone_mock = require('timezone-mock');
+const SunEvents = require('../lib/sunevents');
+const util = require('util');
+const moment = require('moment')
 
 var assert = require('assert');
 
 describe('sunevents', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+  describe('times with timezones', function() {
+    it('should return nadir for Today', function() {
+      timezone_mock.register('US/Pacific')
+      date = new Date('2020-11-30T06:00:00-08:00')
+      sunevent = new SunEvents(37.53, -122.26, {"debug": true, "testMode": true})
+      times = sunevent.getTimes(date, date)
+      nadir = moment(times.nadir)
+      assert.equal(nadir.calendar(date), 'Today at 11:59 PM')
+      timezone_mock.unregister()
     });
   });
 });
