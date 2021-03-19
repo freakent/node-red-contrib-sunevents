bbd3c0df13b6b80995126af392d20a875085f1df
= <=>
2021-03-19 16:34:59 +0000

    Improved taking a copy of msg from saved context

--- spec/sun-events-node-spec.js
+++ spec/sun-events-node-spec.js
@@ -58,6 +58,10 @@ describe('sun-events Node', function () {
             var n2 = helper.getNode("n2");
             n2.on("input", function (msg) {
                 try {
+                    //It should preserve any extra values passed in the msg
+                    expect(msg.misc).toEqual("hello world")
+                    expect(msg.payload.pi).toEqual(3.1459)
+
                     expect(msg.payload.latitude).toEqual(test_lat)
                     expect(msg.payload.longitude).toEqual(test_lng)
                     expect(events.includes(msg.payload.sunevent)).toBeTrue()
@@ -66,8 +70,10 @@ describe('sun-events Node', function () {
                     done(err)
                 }
             });
-            n1.receive({ payload: {latitude: test_lat, longitude: test_lng }});
-            jasmine.clock().tick(1000 * 60 * 60 * 24)
+            for(let day = 0; day < 100; day++) {
+                n1.receive({ misc: "hello world", payload: {pi: 3.1459, latitude: test_lat, longitude: test_lng }});
+                jasmine.clock().tick(1000 * 60 * 60 * 24)
+            }
         });
     });
 
@@ -96,23 +102,26 @@ describe('sun-events Node', function () {
     it('should use lat and long from node configuration if none supplied in msg.payload, payload contains a date', function(done) {
         const test_credentials = { "n1": {'latitude': "51.501364", 'longitude': '-0.1440787' } }
         const flow = [{ id: "n1", type: "sun events", name: "test name", wires: [["n2"]] }, { id: "n2", type: "helper" }];
+        const timestamp = new Date()
+
         helper.load(SunEventsNode, flow, test_credentials, function () {
             let n1 = helper.getNode("n1");
             let n2 = helper.getNode("n2");
             expect(n2).toEqual(jasmine.objectContaining({ type: 'helper' })) // Don't forget to pass "done" into the it function !!!
             n2.on("input", function (msg) {
                 try {
-                    expect(msg.payload.lat).toEqual(test_credentials.latitude, "should match latitude")
-                    expect(msg.payload.lng).toEqual(test_credentials.longitude)
-                    expect(events.includes(msg.payload.sunevent)).toBeTrue(`sunevent ${msg.payload.sunevent} is not known`)
+              //      We didn't pass the lat and lng in so it shouldn't be in the payload
+              //      expect(msg.payload.lat).withContext("should match latitude").toEqual(test_credentials.n1.latitude)
+              //      expect(msg.payload.lng).withContext("should match longitude").toEqual(test_credentials.n1.longitude)
+                    expect(msg.payload).withContext(`payload ${msg.payload} should be a date`).toEqual(timestamp)
+                    expect(events.includes(msg.payload.sunevent)).withContext(`sunevent ${msg.payload.sunevent} is not known`).toBeTrue()
                     done();    
                 } catch(err) {
                     done(err)
                 }
             });
             spyOn(n1, "error")
-            //n1.receive({ payload: {lat: test_credentials.latitude, lng: test_credentials.longitude }});
-            n1.receive({ payload: new Date() }) 
+            n1.receive({ payload: timestamp }) 
             expect(n1.error).not.toHaveBeenCalled()
             jasmine.clock().tick(1000 * 60 * 60 * 24)
         });
@@ -128,16 +137,17 @@ describe('sun-events Node', function () {
             expect(n2).toEqual(jasmine.objectContaining({ type: 'helper' })) // Don't forget to pass "done" into the it function !!!
             n2.on("input", function (msg) {
                 try {
-                    expect(msg.payload.lat).toEqual(test_credentials.latitude, "should match latitude")
-                    expect(msg.payload.lng).toEqual(test_credentials.longitude, "should match longitude")
-                    expect(events.includes(msg.payload.sunevent)).toBeTrue(`sunevent ${msg.payload.sunevent} is not known`)
+//                    We didn't pass the lat and lng in so it shouldn't be in the payload
+//                    expect(msg.payload.lat).withContext("should match latitude").toEqual(test_credentials.n1.latitude)
+//                    expect(msg.payload.lng).withContext("should match longitude").toEqual(test_credentials.n1.longitude)
+                    expect(msg.payload.value).toEqual('some random string')
+                    expect(events.includes(msg.payload.sunevent)).withContext(`sunevent ${msg.payload.sunevent} is not known`).toBeTrue()
                     done();    
                 } catch(err) {
                     done(err)
                 }
             });
             spyOn(n1, "error")
-            //n1.receive({ payload: {lat: test_credentials.latitude, lng: test_credentials.longitude }});
             n1.receive({ payload: 'some random string' })
             expect(n1.error).not.toHaveBeenCalled()
             jasmine.clock().tick(1000 * 60 * 60 * 24)
