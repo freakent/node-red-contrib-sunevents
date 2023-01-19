f28e9ef00b72513f2ef379917973dd3e4103d46f
= <=>
2023-01-19 17:06:06 +0000

    update copyright dates

--- run/sunevents.js
+++ run/sunevents.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013 Freak Enterprises
+ * Copyright 2023 Freak Enterprises
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
